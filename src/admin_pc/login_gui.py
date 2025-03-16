90df90abe37eedafe4b3aa22f4daa599400c7243
Lim <jeongliml2002@gmail.com>
2025-03-16 15:46:16 +0900

    assembling guis completed

--- src/admin_pc/login_gui.py
+++ src/admin_pc/login_gui.py
@@ -2,11 +2,10 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
-import mysql.connector
-from main_gui import MainWindowClass
+# import mysql.connector
 
-logInUi = uic.loadUiType("path/to/login_gui.ui")[0]
-mainUi = uic.loadUiType("path/to/main_gui.ui")[0]
+logInUi = uic.loadUiType("/home/lim/dev_ws/deeplearning-repo-2/src/admin_pc/login_gui.ui")[0]
+mainUi = uic.loadUiType("/home/lim/dev_ws/deeplearning-repo-2/src/admin_pc/main_gui.ui")[0]
 
 
 
@@ -18,12 +17,12 @@ class LogInWindow(QMainWindow, logInUi):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.remote = mysql.connector.connect(
-            host="host_link",
-            user="user_name",
-            password="passwd",
-            database="db_name"
-        )
+        # self.remote = mysql.connector.connect(
+        #     host="host_link",
+        #     user="user_name",
+        #     password="passwd",
+        #     database="db_name"
+        # )
 
         self.lineEdit1.setEchoMode(QLineEdit.Normal)
         self.lineEdit2.setEchoMode(QLineEdit.Password)
@@ -34,20 +33,25 @@ class LogInWindow(QMainWindow, logInUi):
 
 
     def open_main_window(self):
-        """ MySQL 로그인 검증 후 Main.py 실행 """
-        id = self.lineEdit1.text()
-        passwd = self.lineEdit2.text()
+        from main_gui import MainWindowClass
+        # """ MySQL 로그인 검증 후 Main.py 실행 """
+        # id = self.lineEdit1.text()
+        # passwd = self.lineEdit2.text()
 
-        cursor = self.remote.cursor()
-        cursor.execute(f"SELECT * FROM User WHERE id = %s and passwd = SHA2(%s, 256)", (id, passwd))
-        result = cursor.fetchone()
+        # cursor = self.remote.cursor()
+        # cursor.execute(f"SELECT * FROM User WHERE id = %s and passwd = SHA2(%s, 256)", (id, passwd))
+        # result = cursor.fetchone()
 
-        if result:
-            self.main_window = MainWindowClass()  # Main.py의 WindowClass 실행
-            self.main_window.show()  # Main.ui를 실행
-            self.close()  # 현재 로그인 창 닫기
-        else:
-            QMessageBox.warning(self, '로그인 실패', '아이디 또는 비밀번호가 틀렸습니다. 확인 후 다시 시도해주세요.')
+        # if result:
+        #     self.main_window = MainWindowClass()  # Main.py의 WindowClass 실행
+        #     self.main_window.show()  # Main.ui를 실행
+        #     self.close()  # 현재 로그인 창 닫기
+        # else:
+        #     QMessageBox.warning(self, '로그인 실패', '아이디 또는 비밀번호가 틀렸습니다. 확인 후 다시 시도해주세요.')
+
+        self.main_window = MainWindowClass()
+        self.main_window.show()
+        self.close()
 
 
 
