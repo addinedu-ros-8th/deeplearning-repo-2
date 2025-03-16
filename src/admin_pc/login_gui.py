d73137d099e6436e7a288f8217f4c59c0d7bf292
Lim <jeongliml2002@gmail.com>
2025-03-16 21:16:07 +0900

    enable db and login process

--- src/admin_pc/login_gui.py
+++ src/admin_pc/login_gui.py
@@ -14,12 +14,12 @@ class LogInWindow(QMainWindow, logInUi):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        # self.remote = mysql.connector.connect(
-        #     host="host_link",
-        #     user="user_name",
-        #     password="passwd",
-        #     database="db_name"
-        # )
+        self.remote = mysql.connector.connect(
+            host="host_link",
+            user="user_name",
+            password="passwd",
+            database="db_name"
+        )
 
         self.lineEdit1.setEchoMode(QLineEdit.Normal)
         self.lineEdit2.setEchoMode(QLineEdit.Password)
@@ -31,24 +31,21 @@ class LogInWindow(QMainWindow, logInUi):
 
     def open_main_window(self):
         from main_gui import WindowClass
-        # """ MySQL 로그인 검증 후 Main.py 실행 """
-        # id = self.lineEdit1.text()
-        # passwd = self.lineEdit2.text()
+        """ MySQL 로그인 검증 후 Main.py 실행 """
+        id = self.lineEdit1.text()
+        passwd = self.lineEdit2.text()
 
-        # cursor = self.remote.cursor()
-        # cursor.execute(f"SELECT * FROM User WHERE id = %s and passwd = SHA2(%s, 256)", (id, passwd))
-        # result = cursor.fetchone()
+        cursor = self.remote.cursor()
+        cursor.execute(f"SELECT * FROM User WHERE id = %s and passwd = SHA2(%s, 256)", (id, passwd))
+        result = cursor.fetchone()
 
-        # if result:
-        #     self.main_window = MainWindowClass()  # Main.py의 WindowClass 실행
-        #     self.main_window.show()  # Main.ui를 실행
-        #     self.close()  # 현재 로그인 창 닫기
-        # else:
-        #     QMessageBox.warning(self, '로그인 실패', '아이디 또는 비밀번호가 틀렸습니다. 확인 후 다시 시도해주세요.')
+        if result:
+            self.main_window = MainWindowClass()  # Main.py의 WindowClass 실행
+            self.main_window.show()  # Main.ui를 실행
+            self.close()  # 현재 로그인 창 닫기
+        else:
+            QMessageBox.warning(self, '로그인 실패', '아이디 또는 비밀번호가 틀렸습니다. 확인 후 다시 시도해주세요.')
 
-        self.main_window = WindowClass()
-        self.main_window.show()
-        self.close()
 
 
 
