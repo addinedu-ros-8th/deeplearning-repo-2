4e0fe3aa421985c73e7df3efa37f977a657a6004
songwonjoon <djckoreas@naver.com>
2025-04-04 13:02:23 +0900

    장애물 회피, 위험상황 탐지 연동

--- src/video_ai_server/combine_cli_ser.py
+++ src/video_ai_server/combine_cli_ser.py
@@ -4,7 +4,80 @@ import numpy as np
 import cv2
 import os
 from ultralytics import YOLO
+import mediapipe as mp
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+import platform
 import time
+
+if platform.system() == "Darwin":
+    print("your system is mac os")
+    device = torch.device("mps") if torch.backends.mps.is_available() else "cpu"
+else:
+    print("your system is cuda")
+    device = torch.device("cuda") if torch.cuda.is_available() else "cpu"
+
+
+############################################
+# 이상행동 탐지 모델
+############################################
+class LSTM(nn.Module):
+    def __init__(self, num_layers=1):
+        super(LSTM, self).__init__()
+        self.lstm1 = nn.LSTM(103, 128, num_layers, batch_first=True, bidirectional=True)
+        self.layer_norm1 = nn.LayerNorm(256)
+        self.dropout1 = nn.Dropout(0.1)
+
+        self.lstm2 = nn.LSTM(256, 64, num_layers, batch_first=True, bidirectional=True)
+        self.layer_norm2 = nn.LayerNorm(128)
+        self.dropout2 = nn.Dropout(0.1)
+
+        self.lstm3 = nn.LSTM(128, 32, num_layers, batch_first=True, bidirectional=True)
+        self.layer_norm3 = nn.LayerNorm(64)
+        self.dropout3 = nn.Dropout(0.1)
+
+        self.attention = nn.Linear(64, 1)
+        self.fc = nn.Linear(64, 4)
+
+    def forward(self, x):
+        x, _ = self.lstm1(x)
+        x = self.layer_norm1(x)
+        x = self.dropout1(x)
+
+        x, _ = self.lstm2(x)
+        x = self.layer_norm2(x)
+        x = self.dropout2(x)
+
+        x, _ = self.lstm3(x)
+        x = self.layer_norm3(x)
+        x = self.dropout3(x)
+
+        attention_weights = torch.softmax(self.attention(x), dim=1)
+        x = torch.sum(attention_weights * x, dim=1)
+
+        x = self.fc(x)
+        return x
+
+
+############################################
+# 이상행동 데이터셋 처리
+############################################
+class MyDataset(Dataset):
+    def __init__(self, seq_list):
+        self.X = []
+        self.y = []
+        for dic in seq_list:
+            self.y.append(dic['key'])
+            self.X.append(dic['value'])
+
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, index):
+        data = self.X[index]
+        label = self.y[index]
+        return torch.Tensor(np.array(data)), torch.tensor(np.array(int(label)))
 
 
 ############################################
@@ -59,9 +132,67 @@ def receive_video(udp_port, cam_id):
 ############################################
 # 행동분석 함수
 ############################################
-def motionPrediction():
+def motionPrediction(image, poses, yolo_model, lstm_model):
     # 예: "fall_detected", "fire_detected", "normal"
-    return "fall_detected"
+    length = 18
+    detect_cls = 1
+
+    lstm_model.eval()
+    dataset = []
+    status = 'None'
+
+    xyz_list_list = []
+    status_dict = {0: 'normal', 1: 'fighting', 2: 'lying', 3: 'smoking'}
+
+    image = cv2.resize(image, (640, 640))
+
+    # Mediapipe 포즈 추출
+    results = poses.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    xyz_list = []
+
+    if results.pose_landmarks:
+
+        # 포즈 랜드마크 추출 및 그리기
+        for landmark in results.pose_landmarks.landmark:
+            xyz_list.append(landmark.x)
+            xyz_list.append(landmark.y)
+            xyz_list.append(landmark.z)
+
+        # YOLO 박스 예측
+        box_results = yolo_model.predict(image, conf=0.6, verbose=False, show=False)[0].boxes
+        boxes = box_results.xyxy.cpu().tolist()
+        box_class = box_results.cls.cpu().tolist()
+
+        p1x1, p1y1, p1x2, p1y2 = 0, 0, 0, 0
+        p2x1, p2y1, p2x2, p2y2 = 0, 0, 0, 0
+        for idx, cls in enumerate(box_class):
+            if int(cls) == detect_cls:
+                p1x1, p1y1, p1x2, p1y2 = map(int, boxes[0])
+                if len(boxes) > 1:
+                    p2x1, p2y1, p2x2, p2y2 = map(int, boxes[1])
+
+                break
+
+        # YOLO 박스 좌표 정규화 후 추가
+        xyz_list.extend([abs(p1x1 - p2x1) / 640, abs(p1x2 - p2x2) / 640, abs(p1y1 - p2y1) / 640, abs(p1y2 - p2y2) / 640])
+        xyz_list_list.append(xyz_list)
+
+    # 시퀀스 길이에 도달하면 LSTM 예측 수행
+    if len(xyz_list_list) == length:
+        dataset = [{'key': 0, 'value': xyz_list_list}]  # 임시 라벨 0 사용
+        dataset = MyDataset(dataset)
+        dataset_loader = DataLoader(dataset, batch_size=1)
+
+        for data, _ in dataset_loader:
+            data = data.to(device)
+            with torch.no_grad():
+                result = lstm_model(data)
+                _, out = torch.max(result, 1)
+                status = status_dict.get(out.item(), 'Unknown')
+
+        xyz_list_list = []  # 시퀀스 초기화
+
+    return status
 
 
 ############################################
@@ -136,27 +267,22 @@ def load_calibration():
 ############################################
 # 비상 처리
 ############################################
-def handle_emergency(client_socket, stop_action, prev_action):
-    global emergency_mode
-    emergency_mode = True
-    client_socket.send(stop_action.encode('utf-8'))
-    print("emergency STOP")
-    print("녹화시작")
+def handle_emergency(client_socket, stop_action, prev_action, status):
+    global emergency_mode    
 
-    p_predict = motionPrediction()
-    while True:
-        c_predict = motionPrediction()
-        if p_predict != c_predict:
-            print("녹화종료")
-            break
-        time.sleep(1)
-
-    if prev_action is None:
-        client_socket.send(b"FORWARD")
+    if status != "normal":
+        emergency_mode = True
+        client_socket.send(stop_action.encode('utf-8'))
+        print("emergency STOP")
+        print("녹화시작")
+        
     else:
-        client_socket.send(prev_action.encode('utf-8'))
-    
-    emergency_mode = False
+        if prev_action is None:
+            client_socket.send(b"FORWARD")
+        else:
+            client_socket.send(prev_action.encode('utf-8'))
+
+        emergency_mode = False
 
 
 ############################################
@@ -195,6 +321,16 @@ def start_depth_action():
     # YOLO 모델 로드(사람 + 소화기 + 불)
     model = YOLO('../model/merge_best.pt')
 
+    # mediapipe 모델 로드
+    mp_pose = mp.solutions.pose
+    poses = mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.5)
+
+    # LSTM 모델 로드
+    model_path = "/home/pepsi/dev_ws/deeplearning-repo-2/src/video_ai_server/models/lstm_model.pth"
+    lstm_model = LSTM().to(device)
+    lstm_model.load_state_dict(torch.load(model_path, map_location=device))
+    lstm_model.eval()
+
     # 스레드 시작
     thread1 = threading.Thread(target = receive_video, args = (UDP_PORT1, "CAM1"), daemon = True)
     thread2 = threading.Thread(target = receive_video, args = (UDP_PORT2, "CAM2"), daemon = True)
@@ -230,6 +366,9 @@ def start_depth_action():
         dispR = stereoR.compute(grayR, grayL).astype(np.int16)
         filtered = wls_filter.filter(disp, grayL, None, dispR)
         disp_map = filtered.astype(np.float32) / 16.0
+
+        status = motionPrediction(Left_nice, poses, model, lstm_model)
+        threading.Thread(target = handle_emergency, args =(client_socket, stop_action, prev_action, status), daemon = True).start() 
 
         results = model.track(Left_nice, conf = 0.6, persist = True, verbose = False)
         boxes = results[0].boxes if len(results) > 0 else []
@@ -271,13 +410,7 @@ def start_depth_action():
             # 객체 id 및 distance 출력
             label = f"ID:{int(track_id)} {float(distance):.2f}m"
             cv2.rectangle(Left_nice, (x1, y1), (x2, y2), (0,255,0), 2)
-            cv2.putText(Left_nice, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,255), 2)
-
-            # 불 or 사람 감지 시
-            if class_name in ["fire", "person"] and not emergency_mode:
-                ret = motionPrediction()
-                if ret != "normal":
-                    threading.Thread(target = handle_emergency, args =(client_socket, stop_action, prev_action), daemon = True).start()            
+            cv2.putText(Left_nice, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,255,255), 2)           
 
             # roi 범위확인
             in_roi = (x1 < roi_x2 and x2 > roi_x1 and y1 < roi_y2 and y2 > roi_y1)
